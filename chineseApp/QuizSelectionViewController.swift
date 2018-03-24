83768ffb7b1d612d79e408da3cd1d3a1735ff3ec
taberulayu <example@com.com>
2018-03-24 13:46:16 +0900

    [update] refactor

--- chineseApp/QuizSelectionViewController.swift
+++ chineseApp/QuizSelectionViewController.swift
@@ -14,16 +14,43 @@ import Spring
 
 class QuizSelectionViewController: UIViewController {
 
+    enum CatType: Int {
+        case cat1
+        case cat2
+        case cat3
+        case cat4
+        case cat5
+        case cat6
+        case cat7
+        
+        var image: UIImage {
+            switch self {
+            case .cat1:
+                return #imageLiteral(resourceName: "ball1")
+            case .cat2:
+                return #imageLiteral(resourceName: "ball2")
+            case .cat3:
+                return #imageLiteral(resourceName: "ball3")
+            case .cat4:
+                return #imageLiteral(resourceName: "ball4")
+            case .cat5:
+                return #imageLiteral(resourceName: "ball5")
+            case .cat6:
+                return #imageLiteral(resourceName: "ball6")
+            case .cat7:
+                return #imageLiteral(resourceName: "ball7")
+            }
+        }
+        
+    }
+    
     @IBOutlet weak var level1Button: UIButton!
     @IBOutlet weak var level2Button: UIButton!
     @IBOutlet weak var level3Button: UIButton!
     @IBOutlet weak var level4Button: UIButton!
     @IBOutlet weak var level5Button: UIButton!
-    @IBOutlet weak var myLabel: UILabel!
     @IBOutlet weak var springImageView1: SpringImageView!
     @IBOutlet weak var springImageView2: SpringImageView!
-    //    @IBOutlet weak var myImageView1: UIImageView!
-//    @IBOutlet weak var myImageView2: UIImageView!
     var quizVocabrary:Results<WordObj>!
     let screenWidth = UIScreen.main.bounds.size.width
     var levelButtonList:[UIButton]!
@@ -31,7 +58,6 @@ class QuizSelectionViewController: UIViewController {
     var sectionNumber = 0
     //初期の選択ボタンが一番初級レベルなので
     var pointUnit = 10
-//    var currentImageView:UIImageView!
     var currentImageView:SpringImageView!
     @IBOutlet weak var quizStartButton: RoundedRectButton!
     
@@ -39,6 +65,7 @@ class QuizSelectionViewController: UIViewController {
         super.viewDidLoad()
         currentImageView = springImageView1
         
+        // このアプリの購入履歴があるかどうか調べる
         SwiftyStoreKit.retrieveProductsInfo(["com.zakiyamaaaaa.chinesewithcats"]) { result in
             
             if let product = result.retrievedProducts.first {
@@ -58,7 +85,6 @@ class QuizSelectionViewController: UIViewController {
         }
         
         let buttonDefaultImage = #imageLiteral(resourceName: "cat_icon").withRenderingMode(.alwaysTemplate)
-//        springImageView2.transform = CGAffineTransform.init(translationX: screenWidth, y: 0)
         
         levelButtonList = [level1Button,level2Button,level3Button,level4Button,level5Button]
         
@@ -70,10 +96,9 @@ class QuizSelectionViewController: UIViewController {
             button.tag = index
             
         }
-        myLabel.text = quizType.a.rawValue
+        
         level1Button.tintColor = UIColor.red
         level1Button.alpha = 1
-        // Do any additional setup after loading the view.
     }
     
     @objc func levelButtonPushed(sender:UIButton){
@@ -85,28 +110,6 @@ class QuizSelectionViewController: UIViewController {
         sender.alpha = 1
         sender.tintColor = UIColor.red
         selectLevel = sender.tag
-        
-        //選択されたボタンで、ラベルの表示変更
-        switch sender.tag {
-        case 0:
-            myLabel.text = quizType.a.rawValue
-            
-        case 1:
-            myLabel.text = quizType.b.rawValue
-            
-        case 2:
-            myLabel.text = quizType.c.rawValue
-            
-        case 3:
-            myLabel.text = quizType.d.rawValue
-            
-        case 4:
-            myLabel.text = quizType.e.rawValue
-            
-        default:
-            break
-        }
-        
     }
     
     
@@ -119,24 +122,9 @@ class QuizSelectionViewController: UIViewController {
     
     @IBAction func segmentChanged(_ sender: UISegmentedControl) {
         
-        var image:UIImage = UIImage()
+        let catType = CatType(rawValue: sender.selectedSegmentIndex)!
+        let image = catType.image
         sectionNumber = sender.selectedSegmentIndex
-        switch sender.selectedSegmentIndex {
-        case 0:
-            image = #imageLiteral(resourceName: "ball1")
-        case 1:
-            image = #imageLiteral(resourceName: "ball2")
-        case 2:
-            image = #imageLiteral(resourceName: "ball3")
-        case 3:
-            image = #imageLiteral(resourceName: "ball4")
-        case 4:
-            image = #imageLiteral(resourceName: "ball5")
-        case 5:
-            image = #imageLiteral(resourceName: "ball6")
-        default:
-            break
-        }
         
         switch currentImageView {
         case springImageView1:
@@ -148,11 +136,8 @@ class QuizSelectionViewController: UIViewController {
                 self.springImageView2.animation = "zoomIn"
                 self.springImageView2.animate()
                 
-//                self.myImageView1.transform = CGAffineTransform.init(translationX: -self.screenWidth, y: 0)
-//                self.myImageView2.transform = CGAffineTransform.init(translationX: 0, y: 0)
             }, completion: { (Bool) in
                 sender.isEnabled = true
-//                self.springImageView1.transform = CGAffineTransform.init(translationX: self.screenWidth, y: 0)
             })
             currentImageView = springImageView2
             break
@@ -164,11 +149,8 @@ class QuizSelectionViewController: UIViewController {
                 self.springImageView2.animate()
                 self.springImageView1.animation = "zoomIn"
                 self.springImageView1.animate()
-//                self.myImageView2.transform = CGAffineTransform.init(translationX: -self.screenWidth, y: 0)
-//                self.myImageView1.transform = CGAffineTransform.init(translationX: 0, y: 0)
             }, completion: { (Bool) in
                 sender.isEnabled = true
-//                self.springImageView2.transform = CGAffineTransform.init(translationX: self.screenWidth, y: 0)
             })
             currentImageView = springImageView1
         default:
