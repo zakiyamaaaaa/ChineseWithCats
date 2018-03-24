1028a7751b89953b9ab45cd18a2ee91b767fd27c
taberulayu <example@com.com>
2018-03-24 13:46:06 +0900

    [add] custom rounded rect view added
    角丸、影がIBInspectableプロパテイ を持つクラスを作成

---
+++ chineseApp/RoundedRectView.swift
@@ -0,0 +1,79 @@
+//
+//  UIView+Ex.swift
+//  chineseApp
+//
+//  Created by shoichiyamazaki on 2018/03/24.
+//  Copyright © 2018年 shoichiyamazaki. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundedRectView:UIView {
+
+    @IBInspectable var cornerRadius: CGFloat = 20 {
+        didSet {
+            layer.cornerRadius = cornerRadius
+        }
+    }
+
+    @IBInspectable var borderWidth: CGFloat = 0 {
+        didSet {
+            layer.borderWidth = borderWidth
+        }
+    }
+
+    @IBInspectable var borderColor: UIColor = UIColor.darkGray {
+        didSet {
+            layer.borderColor = borderColor.cgColor
+        }
+    }
+    
+    @IBInspectable
+    var shadowRadius: CGFloat {
+        get {
+            return layer.shadowRadius
+        }
+        set {
+            layer.shadowRadius = newValue
+        }
+    }
+    
+    @IBInspectable
+    var shadowOpacity: Float {
+        get {
+            return layer.shadowOpacity
+        }
+        set {
+            layer.shadowOpacity = newValue
+        }
+    }
+    
+    @IBInspectable
+    var shadowOffset: CGSize {
+        get {
+            return layer.shadowOffset
+        }
+        set {
+            layer.shadowOffset = newValue
+        }
+    }
+    
+    @IBInspectable
+    var shadowColor: UIColor? {
+        get {
+            if let color = layer.shadowColor {
+                return UIColor(cgColor: color)
+            }
+            return nil
+        }
+        set {
+            if let color = newValue {
+                layer.shadowColor = color.cgColor
+            } else {
+                layer.shadowColor = nil
+            }
+        }
+    }
+}
+
--- chineseApp/UIView+Ex.swift
+++
@@ -1,9 +0,0 @@
-//
-//  UIView+Ex.swift
-//  chineseApp
-//
-//  Created by shoichiyamazaki on 2018/03/24.
-//  Copyright © 2018年 shoichiyamazaki. All rights reserved.
-//
-
-import Foundation
